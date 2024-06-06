ae734f476ef4c90d7bc81278a41a7ba98bf4b6a3
Victor Liu <victorliu9001@gmail.com>
2024-06-05 22:47:23 -0400

    guild to global

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,9 +1,10 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, guildId, token } = require('./config.json');
+const { clientId, token } = require('./config.json');
 
 const commands = [
+  new SlashCommandBuilder().setName('hello').setDescription('Says hello or something'),
 	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
 	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
 	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
@@ -12,6 +13,6 @@ const commands = [
 
 const rest = new REST({ version: '9' }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+rest.put(Routes.applicationCommands(clientId), { body: commands })
 	.then(() => console.log('Successfully registered application commands.'))
 	.catch(console.error);
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Client, Intents } = require('discord.js');
 const { token } = require('./config.json');
+// const { joinVoiceChannel } = require('@discordjs/voice');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
