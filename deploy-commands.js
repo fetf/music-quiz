11f1b7567614aa9a438ab93b1aa7edfd31b276a0
Richard S. <67170936+RichardSba@users.noreply.github.com>
2024-06-07 03:44:50 +0000

    playlists work

--- deploy-commands.js
+++ deploy-commands.js
@@ -4,15 +4,32 @@ const { Routes } = require('discord-api-types/v9');
 const { clientId, token } = require('./config.json');
 
 const commands = [
+  // General Commands
   new SlashCommandBuilder().setName('hello').setDescription('Says hello or something'),
-	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
-	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
-	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
-	new SlashCommandBuilder().setName('panini').setDescription('panini'),
+  new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
+  new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
+  new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+ 
+  // Music Quiz Commands
+  new SlashCommandBuilder().setName('start-quiz').setDescription('Starts a music quiz'),
+  new SlashCommandBuilder().setName('end-quiz').setDescription('Ends the current music quiz'),
+  new SlashCommandBuilder().setName('skip').setDescription('Skips the current song playing'),
+  new SlashCommandBuilder().setName('playlist').setDescription('Sets the playlist to take songs from').addStringOption(option =>
+    option.setName('id').setDescription('YouTube Playlist ID').setRequired(true)
+  ),
+  new SlashCommandBuilder().setName('quiz-length').setDescription('Sets the length of songs the music quiz is'),
+
+  // Music Player Commands
+  new SlashCommandBuilder().setName('panini').setDescription('panini'),
+  new SlashCommandBuilder().setName('play').setDescription('Play a song from YouTube').addStringOption(option =>
+    option.setName('link').setDescription('YouTube Link').setRequired(true)
+  ),
+  new SlashCommandBuilder().setName('stop').setDescription('Stops/Pauses the current song'),
+  new SlashCommandBuilder().setName('resume').setDescription('Resumes the current song in queue'),
 ]
 	.map(command => command.toJSON());
 
-const rest = new REST({ version: '9' }).setToken(token);
+const rest = new REST({ version: '10' }).setToken(token);
 
 rest.put(Routes.applicationCommands(clientId), { body: commands })
 	.then(() => console.log('Successfully registered application commands.'))
--- main.js
+++ main.js
@@ -11,6 +11,7 @@ const {
   const { GatewayIntentBits } = require("discord-api-types/v9")
   const { Client } = require("discord.js");
   const ytdl = require('ytdl-core');
+  const { Client:Client2 } = require("youtubei");
 
 
   const { token } = require("./config.json")
@@ -24,12 +25,14 @@ const {
    * Create the audio player. We will use this for all of our connections.
    */
   const player = createAudioPlayer();
+  
+  const youtube = new Client2();
 
   player.on('stateChange', (oldState, newState) => {
 	console.log(`Audio player transitioned from ${oldState.status} to ${newState.status}`);
   });
   
-  function playSong() {
+  function playSong(url) {
 	/**
 	 * Here we are creating an audio resource using a sample song freely available online
 	 * (see https://www.soundhelix.com/audio-examples)
@@ -42,24 +45,89 @@ const {
   
 
 
-	const url = 'https://www.youtube.com/watch?v=hUE2DuMP9y8&pp=ygUXcGFuaW5pIGxpbCBuYXMgeCBkYWJhYnk%3D'
-    const stream = ytdl(url, {filter: 'audioonly'});
+	//const url = 'https://www.youtube.com/watch?v=hUE2DuMP9y8&pp=ygUXcGFuaW5pIGxpbCBuYXMgeCBkYWJhYnk%3D'
 
-	const resource = createAudioResource(stream);
+	
+    	const stream = ytdl(url, {filter: 'audioonly'});
+		const resource = createAudioResource(stream);
+		/**
+		 * We will now play this to the audio player. By default, the audio player will not play until
+		 * at least one voice connection is subscribed to it, so it is fine to attach our resource to the
+		 * audio player this early.
+		 */
+		player.play(resource)
+	
+		/**
+		 * Here we are using a helper function. It will resolve if the player enters the Playing
+		 * state within 5 seconds, otherwise it will reject with an error.
+		 */
+		return entersState(player, AudioPlayerStatus.Playing, 5000);
+	
+	
   
-	/**
-	 * We will now play this to the audio player. By default, the audio player will not play until
-	 * at least one voice connection is subscribed to it, so it is fine to attach our resource to the
-	 * audio player this early.
-	 */
-	player.play(resource)
-  
-	/**
-	 * Here we are using a helper function. It will resolve if the player enters the Playing
-	 * state within 5 seconds, otherwise it will reject with an error.
-	 */
-	return entersState(player, AudioPlayerStatus.Playing, 5000);
+	
   }
+
+  let currTimeoutID;
+
+  function playSongList(videos, index, channel) {
+	/**
+	 * Here we are creating an audio resource using a sample song freely available online
+	 * (see https://www.soundhelix.com/audio-examples)
+	 *
+	 * We specify an arbitrary inputType. This means that we aren't too sure what the format of
+	 * the input is, and that we'd like to have this converted into a format we can use. If we
+	 * were using an Ogg or WebM source, then we could change this value. However, for now we
+	 * will leave this as arbitrary.
+	 */
+  
+		if(index > 0){
+			channel.send("It was **" + videos[index-1].title + "** by **" + videos[index-1].channel.name + "**");
+		}
+
+		if(videos.length == index){
+			player.pause();
+			return;
+		}
+	
+    	const stream = ytdl("https://www.youtube.com/watch?v=" + videos[index].id, {filter: 'audioonly'});
+		const resource = createAudioResource(stream);
+		/**
+		 * We will now play this to the audio player. By default, the audio player will not play until
+		 * at least one voice connection is subscribed to it, so it is fine to attach our resource to the
+		 * audio player this early.
+		 */
+		player.play(resource)
+	
+		/**
+		 * Here we are using a helper function. It will resolve if the player enters the Playing
+		 * state within 5 seconds, otherwise it will reject with an error.
+		 */
+		currTimeoutID = setTimeout(playSongList, 15000, videos, index+1, channel);
+		return entersState(player, AudioPlayerStatus.Playing, 5000);
+	
+	
+  
+	
+  }
+
+
+  function shuffle(array) {
+	let currentIndex = array.length;
+  
+	// While there remain elements to shuffle...
+	while (currentIndex != 0) {
+  
+	  // Pick a remaining element...
+	  let randomIndex = Math.floor(Math.random() * currentIndex);
+	  currentIndex--;
+  
+	  // And swap it with the current element.
+	  [array[currentIndex], array[randomIndex]] = [
+		array[randomIndex], array[currentIndex]];
+	}
+  }
+
   
   async function connectToChannel(channel) {
 	/**
@@ -121,7 +189,7 @@ const {
 	 * Try to get our song ready to play for when the bot joins a voice channel
 	 */
 	try {
-		await playSong();
+		//await playSong();
 	  console.log("Song is ready to play!")
 	} catch (error) {
 	  /**
@@ -138,12 +206,17 @@ const {
 	if (!interaction.isCommand()) return;
 	const { commandName } = interaction;
 	if (commandName === 'panini') {
+		player.unpause();
 		const channel = interaction.member?.voice.channel;
 		if (channel) {
 			/**
 			 * The user is in a voice channel, try to connect.
 			 */
 			try {
+				const url = "https://www.youtube.com/watch?v=hUE2DuMP9y8&pp=ygUXcGFuaW5pIGxpbCBuYXMgeCBkYWJhYnk%3D";
+				await playSong(url);
+				
+
 				const connection = await connectToChannel(channel);
 
 				connection.on('stateChange', (oldState, newState) => {
@@ -169,7 +242,125 @@ const {
 			 */
 			void interaction.reply('Join a voice channel then try again!');
 		}
+	} 
+	
+	
+	
+	if (commandName === 'play') {
+		player.unpause();
+		const channel = interaction.member?.voice.channel;
+		if (channel) {
+			/**
+			 * The user is in a voice channel, try to connect.
+			 */
+			try {
+				const url = interaction.options.getString('link');
+				
+				try{
+					await playSong(url);
+				} catch {
+					await interaction.reply('Invalid URL');
+					return;
+				}
+
+
+				const connection = await connectToChannel(channel);
+
+				connection.on('stateChange', (oldState, newState) => {
+					console.log(`Connection transitioned from ${oldState.status} to ${newState.status}`);
+				});
+				/**
+				 * We have successfully connected! Now we can subscribe our connection to
+				 * the player. This means that the player will play audio in the user's
+				 * voice channel.
+				 */
+				connection.subscribe(player);
+				
+				await interaction.reply('Playing now!');
+			} catch (error) {
+				/**
+				 * Unable to connect to the voice channel within 30 seconds :(
+				 */
+				console.error(error);
+			}
+		} else {
+			/**
+			 * The user is not in a voice channel.
+			 */
+			void interaction.reply('Join a voice channel then try again!');
+		}
+	
 	}
+
+	if (commandName === 'stop') {
+		player.pause();
+		await interaction.reply('Stopped!');
+	}
+	if(commandName === 'resume'){
+		player.unpause();
+		await interaction.reply('Resumed!');
+	}
+	if(commandName === 'playlist'){
+		player.unpause();
+		const channel = interaction.member?.voice.channel;
+		if (channel) {
+			/**
+			 * The user is in a voice channel, try to connect.
+			 */
+			try {
+				const playlistId = interaction.options.getString('id');
+				
+				const playlist = await youtube.getPlaylist(playlistId);
+
+				/*let videos = new Array(playlist.videos.items.length);
+
+				for(let i = 0; i < playlist.videos.items.length; i++){
+					videos[i] = "https://www.youtube.com/watch?v=" + playlist.videos.items[i].id;
+				}*/
+
+				let videos = playlist.videos.items.slice();
+				shuffle(videos);
+				
+
+
+				try{
+					await playSongList(videos, 0, interaction.channel);
+				} catch (error) {
+					console.error(error);
+					await interaction.reply('Invalid Playlist');
+					return;
+				}
+
+
+				const connection = await connectToChannel(channel);
+
+				connection.on('stateChange', (oldState, newState) => {
+					console.log(`Connection transitioned from ${oldState.status} to ${newState.status}`);
+				});
+				/**
+				 * We have successfully connected! Now we can subscribe our connection to
+				 * the player. This means that the player will play audio in the user's
+				 * voice channel.
+				 */
+				connection.subscribe(player);
+				
+				await interaction.reply('Playing now!');
+			} catch (error) {
+				/**
+				 * Unable to connect to the voice channel within 30 seconds :(
+				 */
+				console.error(error);
+			}
+		} else {
+			/**
+			 * The user is not in a voice channel.
+			 */
+			void interaction.reply('Join a voice channel then try again!');
+		}
+	
+
+	}
+	
 });
 void client.login(token);
   
