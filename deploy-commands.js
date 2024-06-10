16cba21471dd8dabf12773b43cbee8d8b71e426a
Victor Liu <victorliu9001@gmail.com>
2024-06-09 23:04:52 -0400

    updated commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,25 +1,23 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
+const { Routes } = require('discord-api-types/v10');
 const { clientId, token } = require('./config.json');
 
 const commands = [
-  // General Commands
-  new SlashCommandBuilder().setName('hello').setDescription('Says hello or something'),
-  new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
-  new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
-  new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
- 
+  // Utility Commands
+  new SlashCommandBuilder().setName('ping').setDescription('pong!'),
+
   // Music Quiz Commands
   new SlashCommandBuilder().setName('start-quiz').setDescription('Starts a music quiz').addStringOption(option =>
     option.setName('playlist').setDescription('YouTube Playlist ID for Music Quiz')
+  ).addIntegerOption(option =>
+    option.setName('length').setDescription('Number of songs in Music Quiz')
   ),
   new SlashCommandBuilder().setName('end-quiz').setDescription('Ends the current music quiz'),
   new SlashCommandBuilder().setName('skip').setDescription('Skips the current song playing'),
   new SlashCommandBuilder().setName('playlist').setDescription('Sets the playlist to take songs from').addStringOption(option =>
     option.setName('id').setDescription('YouTube Playlist ID').setRequired(true)
   ),
-  new SlashCommandBuilder().setName('quiz-length').setDescription('Sets the length of songs the music quiz is'),
 
   // Music Player Commands
   new SlashCommandBuilder().setName('panini').setDescription('panini'),
@@ -32,6 +30,10 @@ const commands = [
 	.map(command => command.toJSON());
 
 const rest = new REST({ version: '10' }).setToken(token);
+
+// rest.put(Routes.applicationCommands(clientId), { body: [] })
+// 	.then(() => console.log('Successfully deleted all application commands.'))
+// 	.catch(console.error);
 
 rest.put(Routes.applicationCommands(clientId), { body: commands })
 	.then(() => console.log('Successfully registered application commands.'))
